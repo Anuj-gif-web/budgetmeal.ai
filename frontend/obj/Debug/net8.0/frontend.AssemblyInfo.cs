e74bf6a14b8dd405f49a27cdc7b579b135ec5e1c
Geetesh Pravin Girase <girasege@msu.edu>
2025-07-12 14:44:34 -0400

    added new pages

--- frontend/obj/Debug/net8.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net8.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d76ca7cb772a13881ed82909a8ffa87d5570d277")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec0de9438b7b4f8e575211336cd66f6b7fe688d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
